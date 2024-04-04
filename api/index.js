30997be932af3851facd91441ed741568983b092
Vishal <92736996+vishaljaiswal890@users.noreply.github.com>
2024-04-04 16:01:10 +0530

    upd - 2

--- api/index.js
+++ api/index.js
@@ -148,10 +148,11 @@ app.post("/register", async (req, res) => {
 });
 
 
-app.use('/assets', express.static(path.join(__dirname, 'dist//assets')));
+app.use('/assets', express.static(path.join(__dirname, 'dist/assets')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '//dist//index.html'));
+  // res.sendFile(path.join(__dirname, '//dist//index.html'));
+  res.sendFile('index.html', {root: path.join(__dirname, '/dist')});
 })
 
 const server = app.listen(process.env.PORT || 4000, () => {
